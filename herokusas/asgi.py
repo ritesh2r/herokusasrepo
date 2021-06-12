ef00cf841910f3743f9609e18f405615cf342287
ritesh2r <ritesh2r@gmail.com>
2021-06-12 14:57:29 +0530

    first commit

---
+++ herokusas/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for herokusas project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'herokusas.settings')
+
+application = get_asgi_application()
