ba1fad07028d9d139360b6b7f05b6c05cf8d2448
ritesh2r <ritesh2r@gmail.com>
2021-06-16 15:59:49 +0530

    debug flase

--- herokusas/heroku_settings.py
+++ herokusas/heroku_settings.py
@@ -37,3 +37,4 @@ prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
 ALLOWED_HOSTS = ['heroku1striteshapp.herokuapp.com','riteshm.xyz', 'www.riteshm.xyz' ]
+DEBUG = False
